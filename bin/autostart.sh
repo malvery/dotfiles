3bebf37bb2f8a38409face5a21b1498d4318c10e
malvery <midstoun@gmail.com>
2012-11-18 23:33:01 +0600

    Вс. нояб. 18 23:33:01 YEKT 2012

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -1,16 +1,16 @@
 #~/bin/bg_random_set.sh &
 urxvt -e ~/bin/tmux.sh &
 redshift -l 56.79:60.63 -m vidmode -t 5500:4000 &
-xsetroot -cursor_name left_ptr &
+#xsetroot -cursor_name left_ptr &
 deluge-gtk &
 chromium &
-xfce4-power-manager &
-nm-applet &
-alsa-tray &
-parcellite &
+#xfce4-power-manager &
+#nm-applet &
+kbdd &
+clipit -n &
 
-while true;
-do
-  awsetbg -r /home/malvery/wallpapers/ 
-  sleep 20m
-done &
+#while true;
+#do
+#  awsetbg -r /home/malvery/wallpapers/ 
+#  sleep 20m
+#done &
