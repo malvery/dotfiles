6681e30bf7cdd2cd20e3cc83c387f6566ff83f3d
Anton Zavyalov <a.zavyalov@edgex.ru>
2014-06-11 13:42:02 +0600

    Wed Jun 11 13:42:02 YEKT 2014

---
+++ bin/monaco-powerline-font.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+git clone https://gist.github.com/arkhan/9420125 /tmp
+cp "/tmp/Monaco for Powerline.otf" /usr/share/fonts/OTF/
---
+++ bin/vundle.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir ~/.vim/bunydle/
+git clone https://github.com/gmarik/vundle.git ~/.vim/bunydle/vundle
