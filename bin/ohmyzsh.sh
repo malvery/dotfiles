ca7cb1b460325947cef61829dc39e3f28c70d172
malvery <fort.malvery@gmail.com>
2014-06-15 01:10:09 +0600

    Sun Jun 15 01:10:09 YEKT 2014

--- bin/ohmyzsh.sh
+++ bin/ohmyzsh.sh
@@ -4,3 +4,4 @@
 #git clone git://github.com/zsh-users/zsh-syntax-highlighting.git .zsh/zsh-syntax-highlighting
 curl -L http://install.ohmyz.sh | sh
 cp ~/sync/home/.oh-my-zsh/themes/local.zsh-theme ~/.oh-my-zsh/themes/
+cp ~/sync/home/.zshrc ~/.zshrc
