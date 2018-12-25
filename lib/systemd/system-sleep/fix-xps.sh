da2cf7300daba52af5cad2bab4af85fb8f119c21
Anton Zavalov <malvery@xps9570.localdomain>
2018-12-26 00:25:15 +0300

    xps9570: use nvidia-xrun-pm instead bbswitch

--- lib/systemd/system-sleep/fix-xps.sh
+++ lib/systemd/system-sleep/fix-xps.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 case $1/$2 in
   pre/*)
-		rmmod bbswitch
+		#rmmod bbswitch
 		systemctl stop i8kmon.service NetworkManager.service
     ;;
   post/*)
-		modprobe bbswitch
+		#modprobe bbswitch
 		systemctl start i8kmon.service NetworkManager.service
+		
+		# fix nvidia card power
+		tee /sys/bus/pci/rescan <<<1
+		sleep 1
+		tee /sys/bus/pci/devices/0000:01:00.0/remove <<<1
+		tee /sys/bus/pci/devices/0000:00:01.0/power/control <<<auto
     ;;
 esac
