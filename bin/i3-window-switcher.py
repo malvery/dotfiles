d76c720e0e8aaa4099adda41affe62ec082a27b9
Anton Zavalov <fort.malvery@gmail.com>
2019-04-16 23:55:28 +0300

    i3-window-switcher: fix

--- bin/i3-window-switcher.py
+++ bin/i3-window-switcher.py
@@ -2,74 +2,92 @@
 import setproctitle
 import signal
 import i3ipc
-import time
+
+################################################################################
+
+
+class RecentClients(object):
+    curr = None  # last current window
+    prev = None  # previous window
+
+    def __init__(self, workspace):
+        # save workspace index
+        self.workspace = workspace
+
 
 setproctitle.setproctitle('i3-window-switcher')
+switcher_containers = dict()
 
-########################################################################################################################
-
-
-class WorkspaceClients(object):
-    current = None
-    recent = None
+################################################################################
 
 
-switcher_clients = dict()
+def get_switcher_item(i3):
+    w_tree = i3.get_tree()
+    focused = w_tree.find_focused()
+    workspace_n = focused.workspace().num
+
+    s_container = switcher_containers.get(workspace_n)
+    if s_container.curr is None:
+        s_container.curr = focused.id
+
+    return s_container, focused, w_tree
+
+
+def switch_to_recent(i3):
+    s_container, focused, w_tree = get_switcher_item(i3=i3)
+
+    if s_container.prev is not None:
+        result = i3.command(
+            '[con_id="%s"] focus' % s_container.prev
+        )
+        # force switch
+        if result[0].get('success') is not True:
+            i3.command('focus right')
+
+    else:
+        # empty history
+        i3.command('focus right')
+
+
+################################################################################
+
 i3_conn_commands = i3ipc.Connection()
 
 
 def receiveSignal(signalNumber, frame):
-    print('Received:', signalNumber)
-
-    print('\nrecent_client')
-    focused = i3_conn_commands.get_tree().find_focused()
-    workspace_n = focused.workspace().num
-    workspace = switcher_clients.get(workspace_n)
-
-    if workspace is None:
-        switcher_clients[workspace_n] = WorkspaceClients()
-        switcher_clients[workspace_n].current = focused.id
-        i3_conn_commands.command('focus right')
-
-    elif workspace.recent is not None:
-        print('[con_id="%s"] focus' % workspace.recent)
-        result = i3_conn_commands.command('[con_id="%s"] focus' % workspace.recent)
-        if result[0].get('success') != True:
-            i3_conn_commands.command('focus right')
-
-    else:
-        print('empty workspace history')
-        i3_conn_commands.command('focus right')
-
-    return
+    switch_to_recent(i3=i3_conn_commands)
 
 
 signal.signal(signal.SIGUSR1, receiveSignal)
 
+################################################################################
+
 
 def on_window_focus(i3, e):
-    focused = i3.get_tree().find_focused()
-    workspace_n = focused.workspace().num
-    workspace = switcher_clients.get(workspace_n)
-
-    if not workspace:
-        workspace = WorkspaceClients()
-        switcher_clients[workspace_n] = workspace
-
-    # current_focus = focused.window_class
+    s_container, focused, w_tree = get_switcher_item(i3=i3)
     window_id = focused.id
-    if workspace.current is None:
-        workspace.current = window_id
 
-    if workspace.current != window_id:
-        workspace.recent = workspace.current
-        workspace.current = window_id
+    # check prev window workspace
+    if s_container.prev:
+        w_prev = w_tree.find_by_id(s_container.prev)
+        if w_prev is not None and w_prev.workspace().num != s_container.workspace:
+            # reset prev client
+            s_container.prev = None
 
-    print(workspace_n, workspace.current, workspace.recent)
+    # swap last_curr and recent
+    if s_container.curr != window_id:
+        s_container.prev = s_container.curr
+        s_container.curr = window_id
 
 
+# init switcher containers
+for i in range(1, 10):
+    switcher_containers[i] = RecentClients(workspace=i)
+
+# subscribe "window::focus"
 i3_conn_events = i3ipc.Connection()
 i3_conn_events.on("window::focus", on_window_focus)
+
 try:
     i3_conn_events.main()
 except KeyboardInterrupt:
