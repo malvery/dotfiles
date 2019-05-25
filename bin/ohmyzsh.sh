7bfb1cb68064950ac825e8526de00a401bef11d3
Anton Zavyalov <anton.zavyalov@iqoption.com>
2019-05-25 14:47:12 +0300

    update config

--- bin/ohmyzsh.sh
+++ bin/ohmyzsh.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 curl -L http://install.ohmyz.sh | sh
-cp ~/sync/home/.oh-my-zsh/themes/local.zsh-theme ~/.oh-my-zsh/themes/
-cp ~/sync/home/.zshrc ~/.zshrc
+cp ~/src/dotfiles/home/.oh-my-zsh/themes/local.zsh-theme ~/.oh-my-zsh/themes/
+cp ~/src/dotfiles/home/.zshrc ~/.zshrc
