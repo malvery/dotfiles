4a170ac55fb291a013acbf360f9d930f63eea1cb
Anton Zavalov <fort.malvery@gmail.com>
2019-06-21 01:38:49 +0300

    sway: update dialogs

---
+++ bin/wlr-shot.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+if [[ -z $WAYLAND_DISPLAY ]]; then
+	(>&2 echo Wayland is not running)
+	exit 1
+fi
+
+SHOT_DIR=$(xdg-user-dir PICTURES)
+SHOT_TIME=$(date "+${SWAYSHOT_DATEFMT:-%F_%H-%M-%S}")
+SHOT_PATH="$SHOT_DIR"/screenshot_${SHOT_TIME}.png
+
+# take screenshot
+case "$1" in
+	-r)
+		grim -g "$(slurp -w 3 -d)" "$SHOT_PATH"
+		;;		
+	*)
+		grim -o "$(swaymsg --type get_outputs --raw | jq --raw-output '.[] | select(.focused) | .name')" "$SHOT_PATH"
+		;;
+esac
+echo $SHOT_PATH
+
+# upload to imgur
+if [ "$2" == "-u" ]; then
+	imgur $SHOT_PATH | head -1 | wl-copy -n -f
+fi
+
