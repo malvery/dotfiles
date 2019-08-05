fc77eb1912bbe72b29262df9c616673ccc0c505a
Anton Zavyalov <anton.zavyalov@iqoption.com>
2019-08-05 17:30:03 +0300

    update config

--- home/.config/awesome/host_conf.lua
+++ home/.config/awesome/host_conf.lua
@@ -11,7 +11,7 @@ local xdg_menu = require("archmenu")
 local HOSTNAME = helpers.hostname
 
 local APPS = {
-	["terminal"]		=	"termite",
+	["terminal"]		=	"lxterminal",
 	["lock_manager"]	=	"slock",
 	["file_manager"]	=	"pcmanfm",
 	["browser"]			=	"firefox",
