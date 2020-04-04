2f44a0acb69b9704dac52bd08f84822e641f5e47
Anton Zavalov <fort.malvery@gmail.com>
2020-04-05 02:47:12 +0500

    sway: update color theme

--- home/.config/sway/run.sh
+++ home/.config/sway/run.sh
@@ -3,6 +3,7 @@
 dbus-update-activation-environment --systemd DISPLAY
 eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
 export SSH_AUTH_SOCK
+export XDG_CURRENT_DESKTOP=sway
 
 set -E
 sway 
