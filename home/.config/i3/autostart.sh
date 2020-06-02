6295b76286854e5bcdc8ebb589952deabdbad022
Anton Zavalov <fort.malvery@gmail.com>
2020-06-02 12:22:13 +0500

    update sway / i3 config

--- home/.config/i3/autostart.sh
+++ home/.config/i3/autostart.sh
@@ -4,8 +4,6 @@ xsettingsd &
 nitrogen --restore &
 dunst &
 
-#xss-lock -- i3lock -c 222f42 & 
-#xss-lock -n /usr/lib/xsecurelock/dimmer -l -- xsecurelock.sh &
 xss-lock -- xsecurelock.sh & 
 libinput-gestures-setup start &
 /usr/lib/gpaste/gpaste-daemon &
@@ -14,5 +12,7 @@ blueman-applet &
 nm-applet &
 redshift-gtk &
 
+alacritty -e tmux-s-main.sh &
 thunderbird &
 telegram-desktop &
+
