9ec2ee817ca97a69ad9e1db9af2f06a3034e99ed
Anton Zavalov <fort.malvery@gmail.com>
2021-07-15 00:40:42 +0500

    cleanup

--- bin/ohmyzsh_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-curl -L http://install.ohmyz.sh | sh
-cp ~/src/dotfiles/home/.oh-my-zsh/themes/local.zsh-theme ~/.oh-my-zsh/themes/
-cp ~/src/dotfiles/home/.zshrc ~/.zshrc
--- bin/rofi-windows-switcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rofi \
-    -show windowcd  \
-    -kb-cancel "Alt+Escape,Escape" \
-    -kb-accept-entry "!Alt-Tab,Return"\
-    -kb-row-down "Alt+Tab,Alt+Down" \
-    -kb-row-up "Alt+Shift+Tab,Alt+Up" &
-
