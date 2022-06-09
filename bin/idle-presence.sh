b717d983eee61f345c4f094314ab2a9736eed1ff
Anton Zavyalov <a.a.zavyalov@tinkoff.ru>
2022-06-09 17:54:37 +0500

    update

--- bin/idle-presence.sh
+++ bin/idle-presence.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 #requires:
 # 'xprintidle' for inactivity check (in ms)
 # 'rand' for generating random number (screen resolution)
@@ -8,7 +9,7 @@
 # 800 / 600: your screen resolution, at at least the moving range for the mouse pointer
 
 while :; do
-    if  [ $(xprintidle) -gt 180000 ]
+    if  [[ $(xprintidle) -gt 180000 ]] && ! [[ $(pgrep xsecurelock) ]]
     then
         xdotool mousemove `rand -M 1920` `rand -M 1080`;
     fi
