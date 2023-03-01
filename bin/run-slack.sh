aa3fab075e5313abe27c06aa05ecb43a65da49bd
Anton Zavyalov <a.a.zavyalov@tinkoff.ru>
2023-03-01 12:23:37 +0500

    fix

--- bin/run-slack.sh
+++ bin/run-slack.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 flatpak run com.slack.Slack --ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer $@
+# slack --ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer $@
