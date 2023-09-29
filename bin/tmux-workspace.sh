c7af2073428b627fbc9c2c6aef856808df237c58
Anton Zavyalov <fort.malvery@gmail.com>
2023-09-29 12:05:02 +0500

    update

--- bin/tmux-workspace.sh
+++ bin/tmux-workspace.sh
@@ -3,16 +3,18 @@
 if [ -z "$1" ]; then
   DEFAULT=main
   tmux attach -t $DEFAULT && exit 0
-  systemd-run --user --scope tmux new-session -d -s $DEFAULT
+  # systemd-run --user --scope tmux new-session -d -s $DEFAULT
+  tmux new-session -d -s $DEFAULT
 
-  tmux split-window -h
+  # tmux split-window -h
   # tmux split-window -v
-  tmux selectp -t 0
+  # tmux selectp -t 0
   tmux -2 attach-session -d
 
 else
   ls $1 &> /dev/null || exit 2
   TARGET=${1%/}
-  tmux attach -t $TARGET || systemd-run --user --scope tmux new -t $TARGET -c $TARGET
+  # tmux attach -t $TARGET || systemd-run --user --scope tmux new -t $TARGET -c $TARGET
+  tmux attach -t $TARGET || tmux new -t $TARGET -c $TARGET
 fi
 
