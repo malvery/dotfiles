814aa1e1e40563a8a72a1d6dba03a4027a901615
Anton Zavyalov <fort.malvery@gmail.com>
2024-03-15 13:01:58 +0500

    bin/chromium-update-dicts.sh

---
+++ bin/chromium-update-dicts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+bdic=(en-US-10-1.bdic ru-RU-3-0.bdic)
+dict=${HOME}/.var/app/com.github.Eloston.UngoogledChromium/config/chromium/Dictionaries/
+
+for i in "${bdic[@]}"
+do
+    curl -L https://chromium.googlesource.com/chromium/deps/hunspell_dictionaries/+/refs/heads/main/${i}?format=TEXT -o /tmp/${i}
+    base64 -d /tmp/${i} > ${dict}/${i}
+    rm /tmp/${i}
+
+    echo "Downloaded: ${i}"
+done
+
+echo "Well done!"
