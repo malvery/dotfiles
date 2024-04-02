19b972bdb918ba52107333fa928dafc4bc6393e0
Anton Zavyalov <fort.malvery@gmail.com>
2024-04-02 23:13:29 +0500

    fix

--- bin/power_consumption.sh
+++ bin/power_consumption.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 SYS_PATH='/sys/class/power_supply'
-BATT_ID='BAT0'
+BAT_ID='BAT0'
 
-if [ "$HOSTNAME" = tpneo14 ]; then
-    POWER_NOW='power_now'
-    MULTIPLIER=1000000
-else
+if [ -f ${SYS_PATH}/${BAT_ID}/current_now ]; then
     POWER_NOW='current_now'
     MULTIPLIER=100000
+else
+    POWER_NOW='power_now'
+    MULTIPLIER=1000000
 fi
 
-B_VOLTAGE=$(cat $SYS_PATH/$BATT_ID/$POWER_NOW)
+B_VOLTAGE=$(cat $SYS_PATH/$BAT_ID/$POWER_NOW)
 B_VOLTAGE=$(echo "scale=1;$B_VOLTAGE / $MULTIPLIER" | bc -l)
 
 echo "${B_VOLTAGE}W"
