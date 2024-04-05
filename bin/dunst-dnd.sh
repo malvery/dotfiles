0796dedcde56f010daa48da47ad241e02b4942f5
Anton Zavyalov <fort.malvery@gmail.com>
2024-04-06 00:55:34 +0500

    update waybar config

--- bin/dunst-dnd.sh
+++ bin/dunst-dnd.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 COUNT=$(dunstctl count waiting)
-ENABLED=
-DISABLED=
-if [ $COUNT != 0 ]; then DISABLED=" $COUNT"; fi
+ENABLED='{"text": "", "alt": "on", "class":"on"}'
+DISABLED='{"text": "", "alt": "off", "class":"off"}'
+
+if [ $COUNT != 0 ];
+then
+    DISABLED=$(printf '{"text": "%s", "alt": "off", "class":"off"}' ${COUNT})
+fi
+
 if dunstctl is-paused | grep -q "false" ; then echo $ENABLED; else echo $DISABLED; fi
