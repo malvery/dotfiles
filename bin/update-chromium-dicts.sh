2b0ba35e27da760d00350cf85e0dbb22208414a3
Anton Zavyalov <fort.malvery@gmail.com>
2024-04-14 20:57:54 +0500

    update

--- bin/update-chromium-dicts.sh
+++ bin/update-chromium-dicts.sh
@@ -2,7 +2,8 @@
 set -e
 
 bdic=(en-US-10-1.bdic ru-RU-3-0.bdic)
-dict=${HOME}/.var/app/com.github.Eloston.UngoogledChromium/config/chromium/Dictionaries/
+# dict=${HOME}/.var/app/com.github.Eloston.UngoogledChromium/config/chromium/Dictionaries/
+dict=${HOME}/.config/chromium/Dictionaries/
 
 for i in "${bdic[@]}"
 do
