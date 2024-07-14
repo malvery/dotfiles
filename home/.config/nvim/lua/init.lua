dba35a9c6587bc859a9abd5d5e8fd0b45e35c8fe
Anton Zavyalov <fort.malvery@gmail.com>
2024-07-14 20:19:02 +0500

    fix nvim plug

--- home/.config/nvim/lua/init.lua
+++ home/.config/nvim/lua/init.lua
@@ -23,13 +23,6 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 
 -- gitsigns ===================================================================
 require('gitsigns').setup {
-  signs = {
-      add          = {hl = 'GitSignsAdd'   , text = '+', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-      change       = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-      delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-      topdelete    = {hl = 'GitSignsDelete', text = '!', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-      changedelete = {hl = 'GitSignsChange', text = '~_', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    }
 }
 
 -- commenter ==================================================================
