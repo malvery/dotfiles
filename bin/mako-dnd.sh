013cac8c7418e434b06a42f913199c530cfddc2a
Anton Zavyalov <fort.malvery@gmail.com>
2024-08-26 13:15:58 +0500

    fix missed

---
+++ bin/mako-dnd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+COUNT=$(makoctl list  | jq '.data[0] | length')
+ENABLED='{"text": "", "alt": "on", "class":"on"}'
+DISABLED='{"text": "", "alt": "off", "class":"off"}'
+
+
+if [ $COUNT != 0 ];
+then
+    DISABLED=$(printf '{"text": " %s", "alt": "off", "class":"off"}' ${COUNT})
+fi
+
+if [[ $(makoctl mode | grep dnd) != "dnd" ]]
+then
+    echo $ENABLED
+else
+    echo $DISABLED
+fi
