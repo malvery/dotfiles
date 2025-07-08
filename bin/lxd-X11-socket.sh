4ff0f10284ae7c700f992838da1f23166ce4336d
Anton Zavyalov <fort.malvery@gmail.com>
2025-07-08 13:21:59 +0500

    update

--- bin/lxd-X11-socket.sh
+++ bin/lxd-X11-socket.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 index=${DISPLAY#*:}
 dest=/tmp/.X11-unix/Xc
+# dest=${XDG_RUNTIME_DIR}/Xc
 
 
 if test -e ${dest}
@@ -10,9 +11,9 @@ fi
 
 ln -sf /tmp/.X11-unix/X${index} ${dest}
 
-if [ -n "${XAUTHORITY}" ]
-then
-    ln -sf ${XAUTHORITY} ${XDG_RUNTIME_DIR}/xauth_c
-else
-    touch ${XDG_RUNTIME_DIR}/xauth_c
-fi
+# if [ -n "${XAUTHORITY}" ]
+# then
+#     ln -sf ${XAUTHORITY} ${XDG_RUNTIME_DIR}/xauth_c
+# else
+#     touch ${XDG_RUNTIME_DIR}/xauth_c
+# fi
