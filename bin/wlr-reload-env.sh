3b96bc0e2cba5809e38063b062841e1874a6c390
Anton Zavyalov <fort.malvery@gmail.com>
2025-08-15 12:15:04 +0500

    up

--- bin/wlr-reload-env.sh
+++ bin/wlr-reload-env.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 __restart () {
-    pkill -u ${USER} -f $1
-    eval $@ &
+    pkill -u ${USER} -f $1 && eval $@ &
 }
 
 makoctl reload
-__restart pcmanfm-qt --desktop
-__restart gammastep-indicator
+# __restart pcmanfm-qt --desktop
+# __restart gammastep-indicator
 # __restart waybar
