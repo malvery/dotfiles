e755abf295b8ba421e982afd1854628ff63b630a
Anton Zavyalov <fort.malvery@gmail.com>
2025-08-19 13:27:22 +0500

    back to firefox

---
+++ bin/wlr-dpms.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+LOCK=${XDG_RUNTIME_DIR}/wlr-dpms.lock
+
+case "$1" in
+--off)
+  wlr-randr --json | jq ".[] | select(.enabled==true) | .name" >${LOCK}
+  cat ${LOCK} | xargs -I OUTPUT wlopm --off OUTPUT
+  ;;
+--on)
+  cat ${LOCK} | xargs -I OUTPUT wlopm --on OUTPUT
+  ;;
+*)
+  exit 1
+  ;;
+esac
