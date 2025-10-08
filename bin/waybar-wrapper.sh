da001ccc28120d2e0c4fa2e4225203c40fd26917
Anton Zavyalov <fort.malvery@gmail.com>
2025-10-08 11:18:44 +0500

    up

--- bin/waybar-wrapper.sh
+++ bin/waybar-wrapper.sh
@@ -5,7 +5,7 @@ while true; do
   exit_status=$?
 
   echo "... exit code: ${exit_status}"
-  echo "$(date): ${exit_status}" >> /tmp/waybar-${USER}.exit
+  # echo "$(date): ${exit_status}" >> /tmp/waybar-${USER}.exit
 
 
   if [[ ${exit_status} -ne 0 && ${exit_status} -ne 137 && ${exit_status} -ne 143 && $(pgrep -u ${USER} -x sway) ]]; then
