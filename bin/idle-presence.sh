033bb181e027600f8b238f11bc86c3faf3fd108c
Anton Zavyalov <a.a.zavyalov@breezeventures.io>
2025-12-01 21:59:30 +0500

    up

--- bin/idle-presence.sh
+++ bin/idle-presence.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 while :; do
-  if [[ $(xprintidle) -gt 180000 ]] && ! [[ $(pgrep -U $(whoami) xsecurelock) ]]; then
+  if [[ $(xprintidle) -gt 180000 ]] && ! [[ $(pgrep -U $(whoami) i3lock) ]]; then
     X=$(($RANDOM % 1920 + 1))
     Y=$(($RANDOM % 1080 + 1))
     xdotool mousemove $X $Y
