83e6830a3ee5f964ccd87f1e888ea28c8a58ec1e
Anton Zavyalov <fort.malvery@gmail.com>
2026-02-09 22:50:41 +0500

    wlsunset

--- bin/wlsunset.sh
+++ bin/wlsunset.sh
@@ -1,7 +1,55 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-LOCATION=$(curl -s http://ip-api.com/json?fields=lat,lon)
-LAT=$(echo $LOCATION | jq -r '.lat')
-LON=$(echo $LOCATION | jq -r '.lon')
+UPDATE="pkill -u ${USER} -USR1 py3status"
+# UPDATE="waybar-signal sunset"
 
-wlsunset -l $LAT -L $LON -t 5000
+start() {
+
+  LOCATION=$(curl -s http://ip-api.com/json?fields=lat,lon)
+  LAT=$(echo $LOCATION | jq -r '.lat')
+  LON=$(echo $LOCATION | jq -r '.lon')
+
+  wlsunset -l $LAT -L $LON -t 5000 &
+}
+
+case $1'' in
+
+'stop')
+  pkill -u ${USER} -x wlsunset
+  $UPDATE
+  ;;
+
+'start')
+  start
+  $UPDATE
+  ;;
+
+'toggle')
+  if pkill -u ${USER} -x -0 wlsunset; then
+    pkill -u ${USER} -x wlsunset
+  else
+    start
+  fi
+  $UPDATE
+  ;;
+
+'check')
+  command -v wlsunset
+  exit $?
+  ;;
+esac
+
+if pkill -u ${USER} -x -0 wlsunset; then
+  echo "N"
+else
+  echo "D"
+fi
+
+# if pkill -u ${USER} -x -0 wlsunset; then
+#   class="on"
+#   text="location-based gamma correction"
+# else
+#   class="off"
+#   text="no gamma correction"
+# fi
+# printf '{"alt":"%s","tooltip":"%s"}\n' "$class" "$text"
