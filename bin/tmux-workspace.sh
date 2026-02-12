ffd2567673e97b6ceb6d12ff1bdcbc0aad72935c
Anton Zavyalov <a.a.zavyalov@breezeventures.io>
2026-02-12 11:13:13 +0500

    up

--- bin/tmux-workspace.sh
+++ bin/tmux-workspace.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  DEFAULT=main
-  tmux attach -t $DEFAULT && exit 0
-  systemd-run --user --scope tmux new-session -d -s $DEFAULT
-  # tmux new-session -d -s $DEFAULT
+PREFIX="systemd-run --user --scope"
+
+case "$1" in
+-w)
+  ls $2 &>/dev/null || exit 2
+  TARGET=${2%/}
+
+  tmux attach -t $TARGET || ${PREFIX} tmux new-session -t $TARGET -c $TARGET
+  ;;
+*)
+
+  if [ -z "$1" ]; then
+    TARGET=main
+  else
+    TARGET="$1"
+  fi
+
+  tmux attach -t $TARGET && exit 0
+  ${PREFIX} tmux new-session -d -s $TARGET
 
   # tmux split-window -h
   # tmux split-window -v
   # tmux selectp -t 0
   tmux -2 attach-session -d
 
-else
-  ls $1 &>/dev/null || exit 2
-  TARGET=${1%/}
-  tmux attach -t $TARGET || systemd-run --user --scope tmux new -t $TARGET -c $TARGET
-  # tmux attach -t $TARGET || tmux new -t $TARGET -c $TARGET
-fi
+  ;;
+esac
